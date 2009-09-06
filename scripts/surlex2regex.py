9606b67b29fc787f246fd8ed54fe0248f32f529b
Cody Soyland <codysoyland@gmail.com>
2009-09-06 11:59:22 -0500

    added surlex2regex script

---
+++ scripts/surlex2regex.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from surlex import Surlex
+import sys
+from optparse import OptionParser
+
+def main():
+    parser = OptionParser()
+    parser.set_usage('surlex2regex.py <surlex>')
+    if len(sys.argv) == 1:
+        argv = ['-h']
+    else:
+        argv = sys.argv[1:]
+    options, args = parser.parse_args(argv) 
+    print Surlex(args[0]).translate()
+
+if __name__ == '__main__':
+    main()
