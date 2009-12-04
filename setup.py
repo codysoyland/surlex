a676a461d9ed31cbfbd18bfeea8ce5d3f0d1b0c3
Cody Soyland <codysoyland@gmail.com>
2009-12-03 17:52:20 -0800

    Bumped version from 0.1.1 to 0.1.2.

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ except ImportError:
 
 options = dict(
     name = 'surlex',
-    version = '0.1.1',
+    version = '0.1.2',
     description = 'Simple URL expression translator: alternative to regular expressions for URL pattern matching and data extraction.',
     url = 'http://github.com/codysoyland/surlex/tree/master',
-    download_url = 'http://cloud.github.com/downloads/codysoyland/surlex/surlex-0.1.1.tar.gz',
+    download_url = 'http://cloud.github.com/downloads/codysoyland/surlex/surlex-0.1.2.tar.gz',
     packages = ['surlex'],
     package_dir = {'surlex': 'src/surlex'},
     scripts = ['scripts/surlex2regex.py'],
